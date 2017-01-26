6cb813a87e57238c7b629682e5052ed4c2347579
ramirezfranciscof <ramirezfranciscof@users.noreply.github.com>
2017-01-26 11:33:05 -0300

    Adding meat (#1)
    * Initialized repository with a program and a makefile.
    
    * Added a module and the program uses it. Modified Makefile accordingly.

---
+++ main.f90
@@ -0,0 +1,11 @@
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+program main
+
+  use mymod, only: intnum
+  implicit none
+
+  intnum = 1
+  print*,'this is how I roll and I say: ',intnum
+
+end program
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
